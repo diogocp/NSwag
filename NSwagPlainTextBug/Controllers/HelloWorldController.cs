3adec4ef803f17791b3c1f38103ae19eaa40822f
Diogo Pereira <diogo.pereira@verizonconnect.com>
2021-08-11 20:39:58 +0100

    Log Accept header

--- NSwagPlainTextBug/Controllers/HelloWorldController.cs
+++ NSwagPlainTextBug/Controllers/HelloWorldController.cs
@@ -1,13 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace NSwagPlainTextBug.Controllers
 {
     [ApiController]
     public class HelloWorldController : ControllerBase
     {
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet("hello")]
         public string GetHello()
         {
+            Request.Headers.TryGetValue("Accept", out var accept);
+            _logger.LogInformation("Request with Accept: {accept}", accept);
+
             return "Hello, World!";
         }
     }
